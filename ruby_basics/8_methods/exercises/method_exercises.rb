dc9a222bec75bbf272f9debed0cde9f9819a3e3b
Soumik Mukherjee <170843649+deadMartial@users.noreply.github.com>
2024-12-23 12:23:00 +0530

    Completed methods

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,22 +7,34 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(i)
+  i.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+def common_sports(c,f)
+  c.intersection(f)
+end
 
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#uniq and Array#sort together
+def alphabetical_list(a)
+  a.uniq.sort
+end
 
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+def lucky_number(i=7)
+  "Today's lucky number is #{i}"
+end
 
 
 # method name: #ascii_code
@@ -30,6 +42,9 @@
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
+def ascii_code(a)
+  a.length!=1 ? "Input Error" : a.ord
+end
 
 
 # method name: #pet_pun
@@ -39,10 +54,17 @@
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
+def pet_pun(a)
+  puts a=="cat"? "Cats are purr-fect!" : (a=="dog"? "Dogs are paw-some!" : "I think #{a}s have pet-tential!")
+end
+
+  
 
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
-
+def twenty_first_century?(i)
+  (2001 .. 2100).include?(i)
+end
